651081cf14e52de584c170da13478f76f3052e06
Cassie Jones <>
2021-06-01 14:51:10 +0000

    Updated question and answer order

--- candidate-testing.js
+++ candidate-testing.js
@@ -13,14 +13,14 @@ let correctAnswer = "Sally Ride";
 
 let candidateAnswer;
 let questions = [
+"Who was the first American woman in space? ",
 "True or false: 5 kilometer == 5000 meters? ",
 "(5 + 3)/2 * 10 = ? ",
 'Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2? ',
-"Who was the first American woman in space? ",
 "What is the minimum crew size for the ISS? "
 ];
 
-let correctAnswers = ["true", "40", "Trajectory", "Sally Ride", "3"];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 let numCorrect = 0;
 
@@ -101,7 +101,5 @@ module.exports = {
   gradeQuiz: gradeQuiz,
   runProgram: runProgram
 };
-
-
 
 
